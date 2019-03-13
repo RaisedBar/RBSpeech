0b6999f5f7ef3661cde5760095a5604f6d1e3b21
Sean Farrow <sean.farrow@seanfarrow.co.uk>
2019-03-13 05:30:27 +0000

    Add private functions to the CRBSpeechPlugin class to allow loading and unloading of any required API's and to check whether the API's required by an assistive technology product has been loaded.

--- src/RBSpeech.Common/RBSpeechPlugin.h
+++ src/RBSpeech.Common/RBSpeechPlugin.h
@@ -11,5 +11,9 @@ public:
 	virtual HRESULT Silence() = 0;
 	virtual HRESULT SpeakText(BSTR text, VARIANT_BOOL silence) = 0;
 	virtual HRESULT BrailleText(BSTR text) = 0;
+private:
+	virtual HRESULT IsAPILoaded() = 0;
+	virtual HRESULT LoadAPI() = 0;
+	virtual HRESULT UnloadAPI() = 0;
 };
 #endif // !RBSPEECH_PLUGIN_H

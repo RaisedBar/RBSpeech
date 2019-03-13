4a30d60fed865e5064b181442cdf9444a19907d0
Sean Farrow <sean.farrow@seanfarrow.co.uk>
2019-03-13 04:56:09 +0000

    Add a pair of header/cpp files to hold functions we need to integrate with the O/S. Also, add a skeleton function allowing us to check whether the screen reader flag is set.

---
+++ src/RBSpeech.Common/OSFunctions.cpp
@@ -0,0 +1,7 @@
+#include "stdafx.h"
+#include "OSFunctions.h"
+
+HRESULT RaisedBar::RBSpeech::OSFunctions::IsScreenReaderActive()
+{
+	return S_FALSE;
+}
---
+++ src/RBSpeech.Common/OSFunctions.h
@@ -0,0 +1,17 @@
+#ifndef OS_FUNCTIONS_H
+#define OS_FUNCTIONS_H
+#pragma once
+#include "stdafx.h"
+
+namespace RaisedBar
+{
+	namespace RBSpeech 
+{
+		namespace OSFunctions
+		{
+			HRESULT IsScreenReaderActive();
+
+}
+	}
+}
+#endif // !OS_FUNCTIONS_H

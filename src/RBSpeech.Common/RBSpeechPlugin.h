49be87ac6160d1e076f5643823fd0c1626032b15
Sean Farrow <sean.farrow@seanfarrow.co.uk>
2019-03-13 04:30:08 +0000

    Update the CRBSpeechPlugin class with functions that provide basic plugin services.

--- src/RBSpeech.Common/RBSpeechPlugin.h
+++ src/RBSpeech.Common/RBSpeechPlugin.h
@@ -2,8 +2,14 @@
 #define RBSPEECH_PLUGIN_H
 #pragma once
 #include "stdafx.h"
+
 public class CRBSpeechPlugin
 {
-
+public:
+	virtual HRESULT IsPluginForAnAssistiveTechnology() = 0;
+	virtual HRESULT IsProductActive() = 0;
+	virtual HRESULT Silence() = 0;
+	virtual HRESULT SpeakText(BSTR text, VARIANT_BOOL silence) = 0;
+	virtual HRESULT BrailleText(BSTR text) = 0;
 };
 #endif // !RBSPEECH_PLUGIN_H

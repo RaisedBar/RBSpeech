d9fae9a97059f1f0fd0c8cceff79f890eec809b8
Sean Farrow <sean.farrow@seanfarrow.co.uk>
2019-10-23 11:59:10 +0100

    Add a skeleton plugin for SAPI5.

---
+++ src/Plugins/RBSpeech.Plugins.SAPI5/RBSpeechSAPI5Plugin.cpp
@@ -0,0 +1,48 @@
+#include "pch.h"
+#include "RBSpeechSAPI5Plugin.h"
+using namespace std;
+
+HRESULT RaisedBar::RBSpeech::Plugins::CRBSpeechSAPI5Plugin::IsPluginForAnAssistiveTechnology()
+{
+	return E_NOTIMPL;
+}
+
+HRESULT RaisedBar::RBSpeech::Plugins::CRBSpeechSAPI5Plugin::IsProductActive()
+{
+	return E_NOTIMPL;
+}
+
+HRESULT RaisedBar::RBSpeech::Plugins::CRBSpeechSAPI5Plugin::Silence()
+{
+	return E_NOTIMPL;
+}
+
+HRESULT RaisedBar::RBSpeech::Plugins::CRBSpeechSAPI5Plugin::SpeakText(BSTR text, VARIANT_BOOL silence)
+{
+	return E_NOTIMPL;
+}
+
+HRESULT RaisedBar::RBSpeech::Plugins::CRBSpeechSAPI5Plugin::BrailleText(BSTR text)
+{
+	return E_NOTIMPL;
+}
+
+optional<wstring> RaisedBar::RBSpeech::Plugins::CRBSpeechSAPI5Plugin::GetAssistiveTechnologyExecutable()
+{
+	return nullopt;
+}
+
+				HRESULT RaisedBar::RBSpeech::Plugins::CRBSpeechSAPI5Plugin::IsAPILoaded()
+{
+					return E_NOTIMPL;
+}
+
+HRESULT RaisedBar::RBSpeech::Plugins::CRBSpeechSAPI5Plugin::LoadAPI()
+{
+	return E_NOTIMPL;
+}
+
+HRESULT RaisedBar::RBSpeech::Plugins::CRBSpeechSAPI5Plugin::UnloadAPI()
+{
+	return E_NOTIMPL;
+}
---
+++ src/Plugins/RBSpeech.Plugins.SAPI5/RBSpeechSAPI5Plugin.h
@@ -0,0 +1,38 @@
+#ifndef RBSPEECH_SAPI5_PLUGIN_H
+#define RBSPEECH_SAPI5_PLUGIN_H
+#pragma once
+#include "pch.h"
+#include "RBSpeechPlugin.h"
+//C++ include files.
+#include <memory>
+
+namespace RaisedBar {
+	namespace RBSpeech {
+		namespace Plugins {
+			class CRBSpeechSAPI5Plugin : public CRBSpeechPlugin
+			{
+			public:
+				HRESULT IsPluginForAnAssistiveTechnology();
+				HRESULT IsProductActive();
+				HRESULT Silence();
+				HRESULT SpeakText(BSTR text, VARIANT_BOOL silence);
+				HRESULT BrailleText(BSTR text);
+				std::optional<std::wstring> GetAssistiveTechnologyExecutable();
+				//Factory method.
+				static std::shared_ptr<CRBSpeechSAPI5Plugin> create() {
+					return std::make_shared<CRBSpeechSAPI5Plugin>();
+				}
+
+			private:
+				HRESULT IsAPILoaded();
+				HRESULT LoadAPI();
+				HRESULT UnloadAPI();
+
+				//private fields.
+			private:
+						bool isAPILoaded = false;
+			};
+		}
+	}
+}
+#endif

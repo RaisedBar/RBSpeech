16bc21f5e4f60151adddb27c22b500d54f933b3f
Sean Farrow <sean.farrow@seanfarrow.co.uk>
2021-05-18 01:16:02 +0100

    More reformatting ofthe Dolphin plugin source file.

--- src/Plugins/RBSpeech.Plugins.Dolphin/RBSpeechDolphinPlugin.cpp
+++ src/Plugins/RBSpeech.Plugins.Dolphin/RBSpeechDolphinPlugin.cpp
@@ -59,6 +59,7 @@ HRESULT RaisedBar::RBSpeech::Plugins::CRBSpeechDolphinPlugin::SpeakText(BSTR tex
 		hr = Silence();
 		ExitOnFailure(hr, "Unable to silence existing speech.");
 	}
+
 	dwDolphinFunctionReturnCode = DolAccess_Command((VOID *)text, SysStringLen(text), DOLAPI_COMMAND_SPEAK);
 	ExitIfValueGreaterThanSuppliedValue(dwDolphinFunctionReturnCode, DOLACCESS_SUCCESS, hr, S_FALSE, "Unable to speak text through the current Dolphin product.");
 LExit:
@@ -87,9 +88,11 @@ HRESULT RaisedBar::RBSpeech::Plugins::CRBSpeechDolphinPlugin::LoadAPI()
 	path DolphinDllFileName;
 	hr = IsAPILoaded();
 	ExitOnSuccess(hr, "The Dolphin API is already loaded.");
+
 	hr = PathForCurrentProcess(&sczCurrentPath, NULL);
 	ExitOnFailure(hr, "Failed to get current process path.");
 	DolphinDllFileName = sczCurrentPath;
+
 	if (DolphinDllFileName.has_filename())
 	{
 		DolphinDllFileName.remove_filename();
@@ -97,16 +100,21 @@ HRESULT RaisedBar::RBSpeech::Plugins::CRBSpeechDolphinPlugin::LoadAPI()
 
 	//Add the Dolphin API file name.
 	DolphinDllFileName /= L"dolapi.dll";
+
 	//Check file existence.
 	ExitOnFalse(exists(DolphinDllFileName), hr, E_FILENOTFOUND, "The Dolphin dll file is not found.");
+
 	//The dll file exists, so try to load it.
 	DolphinDllApi.load(DolphinDllFileName.generic_wstring());
 	ExitOnFalse(DolphinDllApi.is_loaded(), hr, S_FALSE, "The Dolphin dll file could not be loaded.");
+
 	//Load the functions we need.
 	ExitOnFalse(DolphinDllApi.has("DolAccess_GetSystem"), hr, S_FALSE, "The Dolphin dll does not export the DolAccess_GetSystem function.");
 	DolAccess_GetSystem = DolphinDllApi.get<DolAccess_GetSystemFunc>("DolAccess_GetSystem");
+
 	ExitOnFalse(DolphinDllApi.has("DolAccess_Action"), hr, S_FALSE, "The Dolphin dll does not export the DolAccess_Action function.");
 	DolAccess_Action = DolphinDllApi.get<DolAccess_ActionFunc>("DolAccess_Action");
+
 	ExitOnFalse(DolphinDllApi.has("DolAccess_Command"), hr, S_FALSE, "The Dolphin dll does not export the DolAccess_Command function.");
 	DolAccess_Command = DolphinDllApi.get<DolAccess_CommandFunc>("DolAccess_Command");
 	isAPILoaded = S_OK;
@@ -120,15 +128,19 @@ HRESULT RaisedBar::RBSpeech::Plugins::CRBSpeechDolphinPlugin::UnloadAPI()
 	HRESULT hr = S_OK;
 	hr = IsAPILoaded();
 	ExitOnFailure(hr, "The Dolphin API is not loaded.");
+
 	ExitOnNull(DolAccess_GetSystem, hr, S_FALSE, "The DolAccess_GetSystem function is already null.");
 	DolAccess_GetSystem = nullptr;
 	ExitOnNotNull(DolAccess_GetSystem, hr, S_FALSE, "The DolAccess_GetSystem function could not be released.");
+
 	ExitOnNull(DolAccess_Action, hr, S_FALSE, "The DolAccess_Action function is already null.");
 	DolAccess_Action = nullptr;
 	ExitOnNotNull(DolAccess_Action, hr, S_FALSE, "The DolAccess_Action function could not be released.");
+
 	ExitOnNull(DolAccess_Command, hr, S_FALSE, "The DolAccess_Command function is already null.");
 	DolAccess_Command = nullptr;
 	ExitOnNotNull(DolAccess_Command, hr, S_FALSE, "The DolAccess_Command function could not be released.");
+
 	ExitOnFalse(DolphinDllApi.is_loaded(), hr, S_FALSE, "The Dolphin dll file is not loaded.");
 	DolphinDllApi.unload();
 	ExitOnTrue(DolphinDllApi.is_loaded(), hr, S_FALSE, "The Dolphin dll is still loaded.");
@@ -148,8 +160,10 @@ HRESULT RaisedBar::RBSpeech::Plugins::CRBSpeechDolphinPlugin::CanDolphinProductS
 	DWORD dwCurrentDolphinProduct = 0;
 	hr = CheckAndLoadAPI();
 	ExitOnFailure(hr, "The Dolphin API could not be loaded.");
+
 	hr = GetActualDolphinProduct(&dwCurrentDolphinProduct);
 	ExitOnFailure(hr, "The current Dolphin product could not be obtained.");
+
 	hr = S_FALSE;
 	if ((IsHalActive() ==S_OK) && (IsLunarPlusActive() ==S_OK) && ((dwCurrentDolphinProduct) ==S_OK))
 	{
@@ -166,6 +180,7 @@ HRESULT RaisedBar::RBSpeech::Plugins::CRBSpeechDolphinPlugin::GetActualDolphinPr
 	DWORD currentDolphinProduct = 0;
 	hr = CheckAndLoadAPI();
 	ExitOnFailure(hr, "The Dolphin API could not be loaded.");
+
 	currentDolphinProduct = DolAccess_GetSystem();
 	*pdwActualProduct = currentDolphinProduct;
 LExit:
@@ -178,8 +193,10 @@ HRESULT RaisedBar::RBSpeech::Plugins::CRBSpeechDolphinPlugin::IsHalActive()
 	DWORD dwCurrentDolphinProduct = 0;
 	hr = CheckAndLoadAPI();
 	ExitOnFailure(hr, "The Dolphin API could not be loaded.");
+
 	hr = GetActualDolphinProduct(&dwCurrentDolphinProduct);
 	ExitOnSpecificValue(dwCurrentDolphinProduct, 0, hr, S_FALSE, "A dolphin product is not running.");
+
 	hr = IsSpecificDolphinProductActive(dwCurrentDolphinProduct, DOLACCESS_HAL);
 	ExitOnFailure(hr, "Hal is not active.");
 LExit:
@@ -192,8 +209,10 @@ HRESULT RaisedBar::RBSpeech::Plugins::CRBSpeechDolphinPlugin::IsLunarPlusActive(
 	DWORD dwCurrentDolphinProduct = 0;
 	hr = CheckAndLoadAPI();
 	ExitOnFailure(hr, "The Dolphin API could not be loaded.");
+
 	hr = GetActualDolphinProduct(&dwCurrentDolphinProduct);
 	ExitOnSpecificValue(dwCurrentDolphinProduct, 0, hr, S_FALSE, "A dolphin product is not running.");
+
 	hr = IsSpecificDolphinProductActive(dwCurrentDolphinProduct, DOLACCESS_LUNARPLUS);
 	ExitOnFailure(hr, "LunarPlus is not active.");
 LExit:
@@ -206,8 +225,10 @@ HRESULT RaisedBar::RBSpeech::Plugins::CRBSpeechDolphinPlugin::IsSuperNovaActive(
 	DWORD dwCurrentDolphinProduct = 0;
 	hr = CheckAndLoadAPI();
 	ExitOnFailure(hr, "The Dolphin API could not be loaded.");
+
 	hr = GetActualDolphinProduct(&dwCurrentDolphinProduct);
 	ExitOnSpecificValue(dwCurrentDolphinProduct, 0, hr, S_FALSE, "A dolphin product is not running.");
+
 	hr = IsSpecificDolphinProductActive(dwCurrentDolphinProduct, DOLACCESS_SUPERNOVA);
 	ExitOnFailure(hr, "SuperNova is not active.");
 	LExit:
